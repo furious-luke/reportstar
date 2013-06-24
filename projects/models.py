8a888d7c6b18f7e216c578f962d0287cefc2cc4a
Luke Hodkinson <furious.luke@gmail.com>
2013-06-24 23:05:18 +1000

    Typeo.

--- projects/models.py
+++ projects/models.py
@@ -7,7 +7,7 @@ class Project(models.Model):
     leader = models.ForeignKey(Subject, null=True, blank=True, related_name='leading')
     name = models.CharField(max_length=256, null=True, blank=True)
     code = models.IntegerField(null=True, blank=True)
-    members = models.ManyToManyField(Subject, null=True, blank=True, realted_name='member_of')
+    members = models.ManyToManyField(Subject, null=True, blank=True, related_name='member_of')
     cpu_usage = models.DecimalField(decimal_places=4, max_digits=10, default=0, verbose_name=_('CPU hour usage'))
     disk_quota = models.DecimalField(decimal_places=4, max_digits=10, default=0)
     disk_usage = models.DecimalField(decimal_places=4, max_digits=10, default=0)
